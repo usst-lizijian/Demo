f664cce2ffc1882c8a08aca2d282a108cdef6d46
usst-lizijian <usstlizijian@163.com>
2018-05-14 23:44:01 +0800

    Demo

---
+++ app/src/main/java/com/example/lbstest/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.lbstest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public LocationClient mLocationClient;
+    private TextView positionText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mLocationClient=new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        setContentView(R.layout.activity_main);
+        positionText=(TextView)findViewById(R.id.position_test_view);
+        List<String> permissionList=new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.READ_PHONE_STATE)!=PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if(!permissionList.isEmpty()){
+            String[] permissions=permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,1);
+        }else{
+            requestLocation();
+        }
+    }
+
+    private void requestLocation(){
+        mLocationClient.start();
+    }
+
+    public void onRequestPermissionsResult(int requestCode,String [] permissions,int[] grantResults){
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0){
+                    for(int result:grantResults){
+                        if(result!=PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this,"必须统一所有权限才能使用本程序",
+                                    Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
+                }else {
+                    Toast.makeText(this,"发生未知错误",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+             default:
+        }
+    }
+
+    public class MyLocationListener implements BDLocationListener{
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            StringBuffer currenPosition=new StringBuffer();
+            currenPosition.append("纬度：").append(bdLocation.getLatitude()).append("\n");
+            currenPosition.append("经度：").append(bdLocation.getLongitude()).append("\n");
+            currenPosition.append("定位方式：");
+            if(bdLocation.getLocType()==BDLocation.TypeGpsLocation){
+                currenPosition.append("GPS");
+            }else if(bdLocation.getLocType()==BDLocation.TypeNetWorkLocation){
+                currenPosition.append("网络");
+            }
+            positionText.setText(currenPosition);
+        }
+    }
+}
